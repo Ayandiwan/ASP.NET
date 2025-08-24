a6b88206d932b1658e6fb22aebee1670fc95b297
Ayandiwan <ayandiwan8769@gmail.com>
2025-08-24 18:42:01 +0530

    Add files via upload

---
+++ oee.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace oddand_even
+{
+    public partial class oee : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            this.WebCustomControl11.Text = this.txtnum.Text;
+
+
+
+        }
+    }
+}
