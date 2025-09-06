040f3f7b21dc714331cd39c583ecf639ea58aa97
Ayandiwan <ayandiwan8769@gmail.com>
2025-09-06 10:40:31 +0530

    Add files via upload

---
+++ ConferenceRegistration.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Internal
+{
+    public partial class ConferenceRegistration : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (fuProof.HasFile)
+            {
+                string filePath = "~/Uploads/" + fuProof.FileName;
+                fuProof.SaveAs(Server.MapPath(filePath));
+
+                DataTable dt = new DataTable();
+                dt.Columns.Add("PaperTitle");
+                dt.Columns.Add("Name");
+                dt.Columns.Add("Email");
+
+                dt.Rows.Add(txtPaperTitle.Text, txtName.Text, txtEmail.Text);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+        }
+    }
+}
