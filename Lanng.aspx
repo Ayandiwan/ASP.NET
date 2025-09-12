0a7c58028c70f0a1713c953854f9f79209e150c9
Ayandiwan <ayandiwan8769@gmail.com>
2025-09-12 21:47:10 +0530

    Add files via upload

---
+++ Lanng.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Lanng.aspx.cs" Inherits="Globalization.Lanng" %>
+
+<!DOCTYPE html>
+<html>
+<head runat="server">
+    <title>Globalization Example</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="ddlLanguage" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlLanguage_SelectedIndexChanged">
+                <asp:ListItem Text="English" Value="en" />
+                <asp:ListItem Text="French" Value="fr" />
+                <asp:ListItem Text="Hindi" Value="Hr" />
+
+            </asp:DropDownList>
+            <br />
+            <br />
+            <asp:Label ID="lblWelcome" runat="server" Font-Size="Large" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Lanng.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Globalization
+{
+    public partial class Lanng : System.Web.UI.Page
+    {
+        protected override void InitializeCulture()
+        {
+            // Get selected language from session or default to English
+            string selectedLanguage = Session["SelectedLanguage"] as string ?? "en";
+
+            // Set culture
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(selectedLanguage);
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(selectedLanguage);
+
+            base.InitializeCulture();
+        }
+
+
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                lblWelcome.Text = Resources.Resource1.Welcome;
+
+                // Set dropdown to selected language
+                ddlLanguage.SelectedValue = Thread.CurrentThread.CurrentCulture.TwoLetterISOLanguageName;
+            }
+
+        }
+
+        protected void ddlLanguage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedLang = ddlLanguage.SelectedValue;
+            Session["SelectedLanguage"] = selectedLang;
+
+            // Reload page to apply culture
+            Response.Redirect(Request.Url.ToString());
+
+        }
+    }
+}
