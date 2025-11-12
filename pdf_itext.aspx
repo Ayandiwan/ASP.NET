d633370206db54b7ee408e4238308e74a5f39831
Ayandiwan <ayandiwan8769@gmail.com>
2025-11-12 19:41:59 +0530

    Add files via upload

---
+++ pdf_itext.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="pdf_itext.aspx.cs" Inherits="itext_demo.pdf_itext" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>iTextSharp PDF Generation</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="text-align:center; margin-top:50px;">
+            <asp:Button ID="Button1" runat="server" Text="Generate PDF" 
+                BackColor="#007bff" ForeColor="White" Font-Bold="True"
+                Height="40px" Width="180px"
+                OnClick="Button1_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ pdf_itext.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Web;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+
+
+
+namespace itext_demo
+{
+    public partial class pdf_itext : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            // Set the response type to PDF
+            Response.ContentType = "application/pdf";
+            Response.AddHeader("content-disposition", "attachment;filename=FirstPDFDocument.pdf");
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+
+            // Create a new PDF document
+            Document document = new Document(iTextSharp.text.PageSize.A4, 25, 25, 30, 30);
+
+            // Link the document to the output stream
+            iTextSharp.text.pdf.PdfWriter.GetInstance(document, Response.OutputStream);
+
+            // Open the document for writing
+            document.Open();
+
+            // Add content
+            document.Add(new iTextSharp.text.Paragraph("Hello World!"));
+            document.Add(new iTextSharp.text.Paragraph("This is a sample PDF created using iTextSharp in ASP.NET."));
+            document.Add(new iTextSharp.text.Paragraph("Generated on: " + DateTime.Now.ToString()));
+
+            // Close the document
+            document.Close();
+
+            // End the response
+            Response.End();
+        }
+    }
+}
