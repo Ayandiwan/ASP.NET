613edcf5c3272af1204ebbc82956b4255cf0133c
Ayandiwan <ayandiwan8769@gmail.com>
2025-11-22 09:40:32 +0530

    Add files via upload

---
+++ WebCustomControl2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace p1
+{
+    [DefaultProperty("Text")]
+    [ToolboxData("<{0}:WebCustomControl2 runat=server></{0}:WebCustomControl2>")]
+    public class WebCustomControl2 : WebControl, INamingContainer
+    {
+
+        TextBox t1;
+        TextBox t2;
+        Button btn;
+        Label result;
+
+        protected override void CreateChildControls()
+        {
+            // TextBox 1
+            t1 = new TextBox();
+            t1.ID = "txt1";
+            this.Controls.Add(t1);
+
+            // Line break
+            this.Controls.Add(new LiteralControl("<br/>"));
+
+            // TextBox 2
+            t2 = new TextBox();
+            t2.ID = "txt2";
+            this.Controls.Add(t2);
+
+            // Line break
+            this.Controls.Add(new LiteralControl("<br/>"));
+
+            // Button
+            btn = new Button();
+            btn.ID = "btnAdd";
+            btn.Text = "Add";
+            btn.Click += Btn_Click;
+            this.Controls.Add(btn);
+
+            // Line break
+            this.Controls.Add(new LiteralControl("<br/>"));
+
+            // Result label
+            result = new Label();
+            result.ID = "lblResult";
+            this.Controls.Add(result);
+        }
+
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            
+            EnsureChildControls();
+
+            int a = Convert.ToInt32(t1.Text);
+            int b = Convert.ToInt32(t2.Text);
+            result.Text = "sum: " + (a + b);
+        }
+
+        protected override void Render(HtmlTextWriter writer)
+        {
+            EnsureChildControls();
+            base.Render(writer);
+        }
+    }
+}
---
+++ WebCustomControl3.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace p1
+{
+    [DefaultProperty("Text")]
+    [ToolboxData("<{0}:WebCustomControl3 runat=server></{0}:WebCustomControl3>")]
+    public class WebCustomControl3 : WebControl,INamingContainer
+    {
+
+        private TextBox t1;
+        private TextBox t2;
+        private Button btn1;
+        private Label lbl1;
+
+
+        protected override void CreateChildControls()
+        {
+            
+            t1 = new TextBox();
+            t1.ID = "txtnum1";
+            
+            this.Controls.Add(t1);
+
+            t2 = new TextBox();
+            t2.ID = "txtnum2";
+
+            this.Controls.Add(t2);
+
+
+            btn1 = new Button();
+            btn1.Text = "Click";
+            btn1.ID = "Ans";
+            btn1.Click += ad;
+            this.Controls.Add(btn1);    
+
+
+
+            lbl1 = new Label();
+            lbl1.ID = "ans2";
+
+            this.Controls.Add (lbl1);
+
+
+        }
+
+
+        private void ad(object sender,EventArgs e)
+        {
+
+            EnsureChildControls();
+
+            int a = Convert.ToInt32(t1.Text);
+            int b = Convert.ToInt32(t2.Text);
+
+            lbl1.Text = "Anser:"+(a+b);
+
+
+
+        }
+        protected override void Render(HtmlTextWriter writer)
+        {
+
+            EnsureChildControls ();
+            base.Render(writer);
+
+        }
+
+        
+    }
+}
