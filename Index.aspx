f3177677801c8971b82d480d77bdc022e29b3d0b
Ayandiwan <ayandiwan8769@gmail.com>
2025-12-02 21:12:13 +0530

    Add files via upload

---
+++ Index.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="WebApplication1.Index" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LINQ to SQL CRUD Example</title>
+</head>
+<body>
+
+    <form id="form1" runat="server">
+        <h2>LINQ to SQL CRUD Example</h2>
+
+        Name:
+        <asp:TextBox ID="txtName" runat="server"></asp:TextBox><br />
+
+        City:
+        <asp:TextBox ID="txtCity" runat="server"></asp:TextBox><br /><br />
+
+        Enter ID (for update/delete):
+        <asp:TextBox ID="txtId" runat="server"></asp:TextBox><br /><br />
+
+        <asp:Button ID="btnInsert" runat="server" Text="Insert" OnClick="btnInsert_Click" />
+        <asp:Button ID="btnUpdate" runat="server" Text="Update" OnClick="btnUpdate_Click" />
+        <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" /><br /><br />
+
+        <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+    </form>
+
+</body>
+</html>
---
+++ Index.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Web.UI;
+
+namespace WebApplication1
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        // ✔ Move DB here (INSIDE class)
+        DataClasses1DataContext db = new DataClasses1DataContext();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+                BindGrid();
+        }
+
+        void BindGrid()
+        {
+            GridView1.DataSource = db.Student_07s;
+            GridView1.DataBind();
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            Student_07 s = new Student_07();
+            s.Name = txtName.Text;
+            s.City = txtCity.Text;
+
+            db.Student_07s.InsertOnSubmit(s);
+            db.SubmitChanges();
+
+            BindGrid();
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+            int id = int.Parse(txtId.Text);
+            var s = db.Student_07s.Single(x => x.Id == id);
+
+            s.Name = txtName.Text;
+            s.City = txtCity.Text;
+
+            db.SubmitChanges();
+            BindGrid();
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var s = db.Student_07s.Single(x => x.Id == id);
+
+            db.Student_07s.DeleteOnSubmit(s);
+            db.SubmitChanges();
+            BindGrid();
+        }
+    }
+}
