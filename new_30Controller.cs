85a7dcd03d19c875e48d9a501dc185f14ff139b8
Ayandiwan <ayandiwan8769@gmail.com>
2025-12-04 10:13:15 +0530

    Add files via upload

---
+++ new_30Controller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using p_28_EF.Models;
+
+namespace p_28_EF.Controllers
+{
+    public class new_30Controller : Controller
+    {
+
+        // GET: new_30
+        public ActionResult Insert()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Insert(Student s)
+        {
+            ViewBag.Message = "Data Inserted Successfully!";
+            return View("Success");
+        }
+    }
+}
